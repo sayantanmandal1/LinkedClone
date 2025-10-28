aa23f9ca33dd15e587c35b02d42f37ff5c77fe60
Sayantan Mandal <msayantan05@gmail.com>
2025-10-28 22:51:01 +0530

    n

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './contexts/**/*.{js,ts,jsx,tsx,mdx}',
+    './hooks/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
